b35235b0ea15e968ea1eff97e49e2185f5a190ae
tuongnbgcc200249 <134251667+tuongnbgcc200249@users.noreply.github.com>
2023-10-20 10:38:11 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php 
-    $conn = mysqli_connect('localhost','root','','shop') 
+    $conn = mysqli_connect('j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','jg3ef3qqve2tow6x','zxtei9r6p6ki2d7j','j992srjrod1hrbl7') 
     or die("Cannot connect database".mysqli_connect_error());
 ?>
