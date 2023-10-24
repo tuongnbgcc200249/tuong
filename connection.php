620bbf648662dc1294c0abbe4bbbc3fc240071a0
tuongnbgcc200249 <134251667+tuongnbgcc200249@users.noreply.github.com>
2023-10-24 08:19:22 +0700

    update

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php 
 
-    $conn = mysqli_connect('i0rgccmrx3at3wv3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','yu12cee3qcouimzm','qoq0wfrtbrjtue9b','m63lh37d94onnzlj') 
+    $conn = mysqli_connect('frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','kbibiqs23o89vcfh','z5aii4bwneyrsm3k','z3a8dj8hyqvde9xt') 
     
   
 ?>
